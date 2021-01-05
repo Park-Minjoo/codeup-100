ae08df53e1b2ecf1d3f372b7a704c86c040d93e1
ParkMinjoo <minjoo@ParkMinjooui-MacBookPro.local>
2021-01-05 16:42:14 +0900

    update

---
+++ 13.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    char a, b;
+    std::cin >> a >> b;
+    // std::cin >> b;
+    std::cout << b << " " <<  a << std::endl;
+    return 0;
+}
---
+++ 14.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a;
+    cin >> a;
+    // std::cin >> b;
+    cout << fixed;
+    cout.precision(3);
+    cout <<  a << endl;
+    return 0;
+}
